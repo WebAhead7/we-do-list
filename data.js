bad2875cb812d810b82f25c9ea4dec9d2e77938c
alaabashiyi <alaa.bashiy@gmail.com>
2020-10-26 14:15:55 +0200

    first

---
+++ data.js
@@ -0,0 +1,5 @@
+const data = [
+  { id: 1, desc: "blahblahblah", check: false },
+  { id: 2, desc: "blahblahblasdfd h", check: false },
+  { id: 3, desc: "blahblajdf sdf shblah", check: false },
+];
